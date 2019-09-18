ff1d2a762fa697acd051254ebe103bc98dc6bcec
Pierre Veelen <pierre@pvln.nl>
2019-09-18 18:49:22 +0200

    updates

--- mqtt/build-baseline.sh
+++ mqtt/build-baseline.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo docker build -f /home/dockerhost/container-apps/mqtt/publisher/baseline \
+sudo docker build -f /home/dockerhost/container-apps/mqtt/baseline \
      -t mqtt-baseline \
      /home/dockerhost/container-apps/mqtt/baseline
