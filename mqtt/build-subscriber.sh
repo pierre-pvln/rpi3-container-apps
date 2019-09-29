0383eb3246b549948f5643092001f1519ee250bf
Pierre Veelen <pierre@pvln.nl>
2019-09-29 16:11:15 +0200

    chmodded + typo's fixed

--- mqtt/build-subscriber.sh
+++ mqtt/build-subscriber.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo docker build -f /home/dockerhost/container-apps/mqtt/publisher/Dockerfile-subscriber \
+sudo docker build -f /home/dockerhost/container-apps/mqtt/subscriber/Dockerfile-subscriber \
      -t mqtt-subscriber \
      /home/dockerhost/container-apps/mqtt/subscriber
